3d030b90cb34fa42402f27fa50a84ef4ef326e63
Marcio Giaxa <marciogiaxa@gmail.com>
2017-04-27 09:47:55 -0300

    Refactor NucleicAcids and Nucleotides
    Removes the responsability from create new NucleicAcids
    from DNA and RNA Enum

--- spec/nucleic_acids_spec.cr
+++ spec/nucleic_acids_spec.cr
@@ -1,11 +1,39 @@
 require "./spec_helper"
 
-include Biocr::Seq::Nucleotide
-
 describe Biocr::Seq::NucleicAcids do
+  describe "Create NucleicAcids" do
+    it "create from 1 Nucleotide String" do
+      NucleicAcids(DNA).new("A").should eq [DNA::A]
+      NucleicAcids(DNA).new("A").should eq [DNA::A]
+      NucleicAcids(DNA).new("a").should eq [DNA::A]
+      NucleicAcids(DNA).new("C").should eq [DNA::C]
+      NucleicAcids(DNA).new("c").should eq [DNA::C]
+      NucleicAcids(DNA).new("G").should eq [DNA::G]
+      NucleicAcids(DNA).new("g").should eq [DNA::G]
+
+      NucleicAcids(RNA).new("A").should eq [RNA::A]
+      NucleicAcids(RNA).new("a").should eq [RNA::A]
+      NucleicAcids(RNA).new("C").should eq [RNA::C]
+      NucleicAcids(RNA).new("c").should eq [RNA::C]
+      NucleicAcids(RNA).new("G").should eq [RNA::G]
+      NucleicAcids(RNA).new("g").should eq [RNA::G]
+    end
+
+    it "create k Nucleotide from String" do
+      NucleicAcids(DNA).new("ACGT").should eq [DNA::A, DNA::C, DNA::G, DNA::T]
+      NucleicAcids(DNA).new("aCgT").should eq [DNA::A, DNA::C, DNA::G, DNA::T]
+
+      NucleicAcids(RNA).new("ACGU").should eq [RNA::A, RNA::C, RNA::G, RNA::U]
+      NucleicAcids(RNA).new("aCgU").should eq [RNA::A, RNA::C, RNA::G, RNA::U]
+
+      NucleicAcids(DNA).new("ACGT").should be_a Biocr::Seq::NucleicAcids(Biocr::Seq::Nucleotide::DNA)
+      NucleicAcids(RNA).new("ACGU").should be_a Biocr::Seq::NucleicAcids(Biocr::Seq::Nucleotide::RNA)
+    end
+  end
+
   describe "#values" do
     it "return all values " do
-      dna = DNA.new("ACGT")
+      dna = NucleicAcids(DNA).new("ACGT")
       dna.values.should eq [1, 2, 4, 8]
       dna.values.should be_a Array(UInt8)
     end
@@ -13,12 +41,21 @@ describe Biocr::Seq::NucleicAcids do
 
   describe "#to_s" do
     it "convert to String" do
-      dna = DNA.new("ACGT")
+      dna = NucleicAcids(DNA).new("ACGT")
       dna.to_s.should eq "ACGT"
     end
     it "convert to string and show the class" do
-      dna = DNA.new("A")
+      dna = NucleicAcids(DNA).new("A")
       dna.inspect.should eq "[ Biocr::Seq::Nucleotide::DNA::A ]"
+    end
+  end
+
+  describe "#errors" do
+    it "Raise error when try to create with a empty space" do
+      expect_raises do
+        NucleicAcids(DNA).new("AC GT")
+        NucleicAcids(RNA).new("AC GU")
+      end
     end
   end
 end
--- src/biocr-seq/nucleic_acids.cr
+++ src/biocr-seq/nucleic_acids.cr
@@ -2,6 +2,12 @@ require "./nucleotide.cr"
 
 module Biocr::Seq
   class NucleicAcids(T) < Array(T)
+    def self.new(s : String) : NucleicAcids(T)
+      nucleotides = NucleicAcids(T).new
+      s.each_char { |c| nucleotides << T.parse(c.to_s) }
+      return nucleotides
+    end
+
     def values : Array(UInt8)
       self.map &.value
     end
